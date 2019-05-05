c66e51e4c871aa7a1d41d9c3f1838b948e6e857e
austinmackillop <austin.mackillop@askuity.com>
2019-05-05 15:51:11 -0400

    Added Hamming sol'n

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,7 @@
 module Hamming (distance) where
 
-distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance :: Eq a => [a] -> [a] -> Maybe Integer
+distance xs ys = if (length xs == length ys) then Just $ distance' xs ys else Nothing
+    where
+        distance' xs ys  = foldl addIfEq 0 $ zip xs ys
+        addIfEq acc (x, y) = if (x == y) then acc else acc + 1
