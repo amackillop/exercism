920727129e8aa2338f3832b66575e47417fff5c0
austinmackillop <austin.mackillop@askuity.com>
2019-05-05 15:58:09 -0400

    modified Hamming sol'n

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -3,5 +3,5 @@ module Hamming (distance) where
 distance :: Eq a => [a] -> [a] -> Maybe Integer
 distance xs ys = if (length xs == length ys) then Just $ distance' xs ys else Nothing
     where
-        distance' xs ys  = foldl addIfEq 0 $ zip xs ys
-        addIfEq acc (x, y) = if (x == y) then acc else acc + 1
+        distance' xs ys  = foldl addIfNotEq 0 $ zipWith (==) xs ys
+        addIfNotEq acc bool = if (bool) then acc else acc + 1
